25aef8921484d3f1ac3ab3ed8fee6849ee6eb5f5
ArturoM21 <josee-arturo1@hotmail.com>
2018-05-22 09:08:41 -0500

    cambie app

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/new entry',(request,response)=>response.render('new entry'));
 
 app.post('/,new entry'),(request,response)=>{
     if(!request.body.title || !request.body.body){
-        response.status(400).send('Las entradas deben de tener u titulo y un m');
+        response.status(400).send('Las entradas deben de tener un titulo y un m');
         return;
     }
     entries.push({
